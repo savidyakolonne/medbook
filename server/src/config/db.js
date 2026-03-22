10c562ba5f7acdeb443f456ae7eca92b529b7fea
savidyakolonne <savidya.kolonne@gmail.com>
2026-03-22 11:44:56 +0530

    fix: database error

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,16 @@
-const {Pool} = require("pg") ; 
-const dotenv = require("dotenv") ;
+import pg from "pg";
+import dotenv from "dotenv";
 
-dotenv.config() ;
+dotenv.config();
+
+const { Pool } = pg;
 
 const pool = new Pool({
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT ,
-    database: process.env.DB_NAME ,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD ,
-}) ; 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
-module.exports = pool ; 
+export default pool;
